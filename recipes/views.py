7831f84012801b84aeb96485ee8b164c41b6a19c
Tiago Martins Andrade <x15_tiago@hotmail.com>
2022-11-24 17:57:34 -0300

    Template Add

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Tiago Martins Andrade',
+    })
+
+
+def about(request):
+    return HttpResponse('About')
+
+
+def contact(request):
+    return HttpResponse('Contact')
