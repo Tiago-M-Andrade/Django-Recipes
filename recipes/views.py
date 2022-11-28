ffec6821c1866ef4edb260df33018631b15920a1
Tiago Martins Andrade <x15_tiago@hotmail.com>
2022-11-28 00:03:49 -0300

    Add 404 Page Errors when not found

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,13 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
+# from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
+    # recipes = get_list_or_404(Recipe.objects.filter(
+    #    is_published=True).order_by('-id'))
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -14,19 +15,17 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        is_published=True, category__id=category_id).order_by('-id')
-    if not recipes:
-        raise Http404('Not Found 😢')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        is_published=True, category__id=category_id).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'category_name': f'{recipes.first().category.name}  - Category |',
+        'category_name': f'{recipes[0].category.name}  - Category |',
     })
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(id=id)
+    recipe = get_object_or_404(Recipe, is_published=True, id=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
